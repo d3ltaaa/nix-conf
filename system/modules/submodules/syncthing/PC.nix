4d60a0617a742d48431829e6152f732ab614c58a
d3ltaaa <hil.falk@protonmail.com>
2025-04-18 12:00:39 +0200

    screenshot

--- system/modules/submodules/syncthing/PC.nix
+++ system/modules/submodules/syncthing/PC.nix
@@ -10,9 +10,9 @@
       "T480" = {
         id = "Z3EA4H3-RNVAKPJ-JIWF4HD-L4AISEX-DUZZ4SV-P3E45GU-AKA3DHG-VYQNRAK";
       };
-      # "T440P" = {
-      #   id = "";
-      # };
+      "T440P" = {
+        id = "CAWY2HI-K3QLENX-QABH4C4-QDGBZAB-GH22BRL-ZB6YBG5-PXVDZTR-4MSF7QY";
+      };
     };
 
     folders = {
@@ -20,7 +20,14 @@
         path = "${variables.userHomeDir}/Dokumente";
         devices = [
           "T480"
-          # "T440P"
+          "T440P"
+        ];
+      };
+      "Bilder" = {
+        path = "${variables.userHomeDir}/Bilder";
+        devices = [
+          "T480"
+          "T440P"
         ];
       };
     };
