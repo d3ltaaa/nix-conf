a401829f872a9ced6e4102f8832d5ef375b3dce9
d3ltaaa <hil.falk@protonmail.com>
2025-04-21 01:25:05 +0200

    redo blue sleep

--- system/modules/submodules/services/tlp.nix
+++ system/modules/submodules/services/tlp.nix
@@ -16,9 +16,9 @@
       PCIE_ASPM_ON_BAT = "powersupersave";
 
       DEVICES_TO_ENABLE_ON_STARTUP = "wifi";
-      DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth nfc wwan";
-      DEVICES_TO_DISABLE_ON_SHUTDOWN = "bluetooth nfc wifi wwan";
-      DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "bluetooth nfc wifi wwan";
+      DEVICES_TO_DISABLE_ON_STARTUP = "nfc wwan";
+      DEVICES_TO_DISABLE_ON_SHUTDOWN = "nfc wifi wwan";
+      DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "nfc wifi wwan";
 
       DEVICES_TO_DISABLE_ON_WIFI_CONNECT = "wwan";
       DEVICES_TO_DISABLE_ON_WWAN_CONNECT = "wifi";
