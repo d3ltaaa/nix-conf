9421d0e0d0542bd272c18c134d85c415af715aa0
d3ltaaa <hil.falk@protonmail.com>
2025-05-01 18:36:39 +0200

    initial passwd

--- system/modules/general.nix
+++ system/modules/general.nix
@@ -19,6 +19,8 @@
     "flakes"
   ];
 
+  users.users."${variables.user}".initialPassword = "${variables.user}";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
