f3bc06ffb0391ba339045474a4cd59d40c4a67de
d3ltaaa <hil.falk@protonmail.com>
2025-05-22 08:14:01 +0200

    fix

--- system/modules/homepage.nix
+++ system/modules/homepage.nix
@@ -54,7 +54,7 @@
             }
             {
               "Homeassistant" = {
-                icon = "homeassistant.png";
+                icon = "home assistant.png";
                 href = "https://homeassistant.d3lta.ipv64.net";
               };
             }
