9bc66d240e42cd097f979c0512d0998f39d68d3d
d3ltaaa <hil.falk@protonmail.com>
2025-06-02 18:18:46 +0200

    syn

--- system/modules/submodules/syncthing/PC.nix
+++ system/modules/submodules/syncthing/PC.nix
@@ -13,6 +13,9 @@
       "T440P" = {
         id = "CAWY2HI-K3QLENX-QABH4C4-QDGBZAB-GH22BRL-ZB6YBG5-PXVDZTR-4MSF7QY";
       };
+      "SERVER" = {
+        id = "OP5RCKE-UFEQ4IT-DRMANC2-425AFHE-RS4PG3Y-35VLH6F-7UJXUIJ-EAVK5A3";
+      };
     };
 
     folders = {
@@ -21,6 +24,7 @@
         devices = [
           "T480"
           "T440P"
+          "SERVER"
         ];
       };
       "Bilder" = {
@@ -28,6 +32,7 @@
         devices = [
           "T480"
           "T440P"
+          "SERVER"
         ];
       };
     };
