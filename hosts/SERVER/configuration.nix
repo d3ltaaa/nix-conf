ddc84c54efc4b425fea96897b50ddbf540a35d4a
d3ltaaaaaa <hil.falk@protonmail.com>
2025-06-24 08:47:43 +0200

    moved homepage-config to host

--- hosts/SERVER/configuration.nix
+++ hosts/SERVER/configuration.nix
@@ -90,7 +90,140 @@ in
       open-webui-server.enable = true;
       vaultwarden-server.enable = true;
       homeassistant-server.enable = true;
-      homepage-server.enable = true;
+      homepage-server = {
+        enable = true;
+        widgets = [
+          {
+            resources = {
+              cpu = true;
+              disk = "/";
+              memory = true;
+            };
+          }
+          {
+            search = {
+              provider = "duckduckgo";
+              target = "_blank";
+            };
+          }
+        ];
+        bookmarks = [
+          {
+            "Bookmarks" = [
+              {
+                "Proton Mail" = [
+                  {
+                    icon = "proton-mail.png";
+                    href = "https://mail.proton.me/u/0/inbox";
+                  }
+                ];
+              }
+              {
+                "Proton Calendar" = [
+                  {
+                    icon = "proton-calendar.png";
+                    href = "https://calendar.proton.me/u/0/";
+                  }
+                ];
+              }
+              {
+                "Github" = [
+                  {
+                    icon = "github-light.png";
+                    href = "https://github.com/";
+                  }
+                ];
+              }
+              {
+                "Youtube" = [
+                  {
+                    icon = "youtube.png";
+                    href = "https://youtube.com/";
+                  }
+                ];
+              }
+              {
+                "ChatGPT" = [
+                  {
+                    icon = "chatgpt.png";
+                    href = "https://chat.openai.com/chat";
+                  }
+                ];
+              }
+              {
+                "HM4Mint" = [
+                  {
+                    icon = "bookstack.png";
+                    href = "https://hm4mint.nrw/hm1/link/HoeherMathem1";
+                  }
+                ];
+              }
+            ];
+          }
+        ];
+        services = [
+          {
+            "Administration" = [
+              {
+                "Proxmox" = {
+                  icon = "proxmox.png";
+                  href = "https://proxmox.${config.settings.general.serverAddress}";
+                };
+              }
+              {
+                "Wireguard" = {
+                  icon = "wireguard.png";
+                  href = "https://wg.${config.settings.general.serverAddress}";
+                };
+              }
+            ];
+          }
+          {
+            "Services" = [
+              {
+                "Syncthing" = {
+                  icon = "syncthing.png";
+                  href = "https://syncthing.${config.settings.general.serverAddress}";
+                };
+              }
+              {
+                "Vaultwarden" = {
+                  icon = "vaultwarden.png";
+                  href = "https://vault.${config.settings.general.serverAddress}";
+                };
+              }
+              {
+                "Homeassistant" = {
+                  icon = "home-assistant.png";
+                  href = "https://homeassistant.${config.settings.general.serverAddress}";
+                };
+              }
+              {
+                "Ollama/Open-Webui" = {
+                  icon = "ollama.png";
+                  href = "https://ollama.${config.settings.general.serverAddress}";
+                };
+              }
+            ];
+          }
+          {
+            "Devices" = [
+              {
+                "Router" = {
+                  icon = "router.png";
+                  href = "http://192.168.2.1";
+                };
+              }
+              {
+                "3D Printer" = {
+                  icon = "mainsail.png";
+                  href = "https://dp.${config.settings.general.serverAddress}";
+                };
+              }
+            ];
+          }
+        ];
+      };
       fileSharing-server = {
         enable = true;
         ip = "192.168.2.12";
--- modules/applications/homepage.nix
+++ modules/applications/homepage.nix
@@ -5,144 +5,30 @@
 }:
 {
   options = {
-    applications.configuration.homepage-server.enable = lib.mkEnableOption "Enables Homepage module";
+    applications.configuration.homepage-server = {
+      enable = lib.mkEnableOption "Enables Homepage module";
+      widgets = lib.mkOption {
+        type = lib.types.listOf lib.types.attrs;
+        default = [ ];
+      };
+      bookmarks = lib.mkOption {
+        type = lib.types.listOf lib.types.attrs;
+        default = [ ];
+      };
+      services = lib.mkOption {
+        type = lib.types.listOf lib.types.attrs;
+        default = [ ];
+      };
+    };
   };
   config = lib.mkIf config.applications.configuration.homepage-server.enable {
     services.homepage-dashboard = {
       enable = true;
       allowedHosts = "${config.settings.networking.staticIp}:8082,home.${config.settings.general.serverAddress}";
       openFirewall = true;
-      widgets = [
-        {
-          resources = {
-            cpu = true;
-            disk = "/";
-            memory = true;
-          };
-        }
-        {
-          search = {
-            provider = "duckduckgo";
-            target = "_blank";
-          };
-        }
-      ];
-      bookmarks = [
-        {
-          "Bookmarks" = [
-            {
-              "Proton Mail" = [
-                {
-                  icon = "proton-mail.png";
-                  href = "https://mail.proton.me/u/0/inbox";
-                }
-              ];
-            }
-            {
-              "Proton Calendar" = [
-                {
-                  icon = "proton-calendar.png";
-                  href = "https://calendar.proton.me/u/0/";
-                }
-              ];
-            }
-            {
-              "Github" = [
-                {
-                  icon = "github-light.png";
-                  href = "https://github.com/";
-                }
-              ];
-            }
-            {
-              "Youtube" = [
-                {
-                  icon = "youtube.png";
-                  href = "https://youtube.com/";
-                }
-              ];
-            }
-            {
-              "ChatGPT" = [
-                {
-                  icon = "chatgpt.png";
-                  href = "https://chat.openai.com/chat";
-                }
-              ];
-            }
-            {
-              "HM4Mint" = [
-                {
-                  icon = "bookstack.png";
-                  href = "https://hm4mint.nrw/hm1/link/HoeherMathem1";
-                }
-              ];
-            }
-          ];
-        }
-      ];
-      services = [
-        {
-          "Administration" = [
-            {
-              "Proxmox" = {
-                icon = "proxmox.png";
-                href = "https://proxmox.${config.settings.general.serverAddress}";
-              };
-            }
-            {
-              "Wireguard" = {
-                icon = "wireguard.png";
-                href = "https://wg.${config.settings.general.serverAddress}";
-              };
-            }
-          ];
-        }
-        {
-          "Services" = [
-            {
-              "Syncthing" = {
-                icon = "syncthing.png";
-                href = "https://syncthing.${config.settings.general.serverAddress}";
-              };
-            }
-            {
-              "Vaultwarden" = {
-                icon = "vaultwarden.png";
-                href = "https://vault.${config.settings.general.serverAddress}";
-              };
-            }
-            {
-              "Homeassistant" = {
-                icon = "home-assistant.png";
-                href = "https://homeassistant.${config.settings.general.serverAddress}";
-              };
-            }
-            {
-              "Ollama/Open-Webui" = {
-                icon = "ollama.png";
-                href = "https://ollama.${config.settings.general.serverAddress}";
-              };
-            }
-          ];
-        }
-        {
-          "Devices" = [
-            {
-              "Router" = {
-                icon = "router.png";
-                href = "http://192.168.2.1";
-              };
-            }
-            {
-              "3D Printer" = {
-                icon = "mainsail.png";
-                href = "https://dp.${config.settings.general.serverAddress}";
-              };
-            }
-          ];
-        }
-      ];
+      widgets = config.applications.configuration.homepage-server.widgets;
+      bookmarks = config.applications.configuration.homepage-server.bookmarks;
+      services = config.applications.configuration.homepage-server.services;
     };
   };
 }
